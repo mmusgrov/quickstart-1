10a77c9e15374066b36b747cf5ca395a0c6a1b76
Michael Musgrove <mmusgrov@redhat.com>
2017-03-31 16:36:59 +0100

    JBTM-2874 GlassFish interop

---
+++ ArjunaJTS/interop/step3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source init.sh
+
+# Start a glassfish server
+cd $QS_DIR
+
+#asadmin start-domain --debug domain1 
+asadmin start-domain domain1
+asadmin set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-1.port=7080
+
