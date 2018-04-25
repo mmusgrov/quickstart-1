0e6e2831b6d06c7e6459bf2e1171534bb9bcf209
Michael Musgrove <michael.musgrove@gmail.com>
2018-04-25 18:29:26 +0100

    swarm lra demo

--- rts/lra-swarm/src/main/java/lra/demo/Resource.java
+++ rts/lra-swarm/src/main/java/lra/demo/Resource.java
@@ -3,11 +3,14 @@ package lra.demo;
 import org.eclipse.microprofile.lra.annotation.LRA;
 import org.eclipse.microprofile.lra.client.LRAClient;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -18,6 +21,19 @@ public class Resource {
     @Inject
     LRAClient lraClient;
 
+    private SpecIT test;
+
+    @PostConstruct
+    private void setup() {
+        SpecIT.beforeClass(lraClient);
+        test = new SpecIT();
+    }
+
+    @PreDestroy
+    private void tearDown() {
+        SpecIT.afterClass();
+    }
+
     @GET
     @Path("/greet")
     @Produces("text/html")
@@ -26,18 +42,28 @@ public class Resource {
     }
 
     @PUT
+    @Path("/test/{name}")
+    public Response runTest(@PathParam("name") String testName) throws Exception {
+        String result = "";
+
+        test.before();
+
+        if ("start".equalsIgnoreCase(testName))
+            result = test.startLRA();
+        else if ("joinLRAViaHeader".equalsIgnoreCase(testName))
+            result = test.joinLRAViaHeader();
+
+        test.after();
+
+        return Response.ok(result).build();
+    }
+
+    @PUT
     @Path("/work")
     @LRA(LRA.Type.REQUIRED)
     public Response activityWithLRA(@HeaderParam(LRAClient.LRA_HTTP_RECOVERY_HEADER) String rcvId,
                                     @HeaderParam(LRAClient.LRA_HTTP_HEADER) String lraId) {
-        System.out.printf("lra: %s%n", lraId);
-        System.out.printf("client: %s%n", lraClient == null ? null : lraClient.getClass().getCanonicalName());
-
-//        assert lraId != null;
-//        Activity activity = addWork(lraId, rcvId);
-
-//        if (activity == null)
-//            return Response.status(Response.Status.EXPECTATION_FAILED).entity("Missing lra data").build();
+        System.out.printf("lra: %s and lraClient: %s%n", lraId, lraClient);
 
         return Response.ok(lraId).build();
     }
